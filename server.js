f9d8b533bd2f892f4f990967bd4bfc51b213da7b
FiamaTrajano93 <77476252+FiamaSantos93@users.noreply.github.com>
2024-09-06 03:14:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,60 +1,17 @@
 const express = require('express');
-const multer = require('multer');
 const path = require('path');
-const fs = require('fs');
-const { Octokit } = require('@octokit/rest');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // Use a porta do ambiente se disponível, senão use 3000
 
-// Configuração do multer para armazenar os arquivos de áudio
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const uploadPath = path.join(__dirname, 'uploads');
-    if (!fs.existsSync(uploadPath)) {
-      fs.mkdirSync(uploadPath);
-    }
-    cb(null, uploadPath);
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  }
-});
+// Servir arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
-const upload = multer({ storage: storage });
-
-// Middleware para servir arquivos estáticos
-app.use(express.static('public'));
-
-// Configuração do Octokit
-const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-
-async function uploadToGitHub(filePath) {
-  const fileContent = fs.readFileSync(filePath, { encoding: 'base64' });
-
-  await octokit.repos.createOrUpdateFileContents({
-    owner: 'FiamaSantos93',
-    repo: 'Audios_Sprint_03',
-    path: `uploads/${path.basename(filePath)}`,
-    message: 'Upload de áudio',
-    content: fileContent
-  });
-}
-
-// Rota para o upload do áudio
-app.post('/upload', upload.single('audioFile'), async (req, res) => {
-  if (!req.file) {
-    return res.status(400).send('Nenhum arquivo foi enviado.');
-  }
-
-  try {
-    await uploadToGitHub(req.file.path);
-    res.status(200).send('Áudio enviado e armazenado com sucesso!');
-  } catch (error) {
-    console.error('Erro ao enviar para o GitHub:', error);
-    res.status(500).send('Erro ao enviar o áudio.');
-  }
+// Rota principal para servir o arquivo HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'botao.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
